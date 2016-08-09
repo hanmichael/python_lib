9f496b171c9abf10a78726e899d16e9a82ae58ee
chenguolin <cgl1079743846@gmail.com>
2016-08-10 00:53:37 +0800

    add string_util

---
+++ py_util/string_util.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+def is_string(obj):
+    """
+    Check obj is str or unicode instance
+
+    @obj: input value
+
+    return True/False
+    """
+    try:
+        return isinstance(obj, (str, unicode))
+    except Exception,e:
+        sys.stderr.write('check obj is string exception:[%s]' % (str(e)))
+        return False
