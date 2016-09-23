3d51f9b3d07624ff9964c8c868acc93935d70585
cgl1079743846@gmail.com <cgl1079743846@gmail.com>
2016-09-24 00:58:44 +0800

    word_segment add cut_with_weight

--- py_util/word_segment.py
+++ py_util/word_segment.py
@@ -7,6 +7,7 @@ import json
 import logging
 import warnings
 import jieba
+import jieba.analyse
 #warnings.filterwarnings('ignore')
 
 FORMAT = '[%(levelname)s] [%(asctime)s] [%(filename)s::%(funcName)s::%(lineno)d] [%(message)s]'
@@ -22,20 +23,41 @@ class WordSegment(object):
 
         每一行分三部分：词语、词频（可省略）、词性（可省略），用空格隔开
         """
-        if user_dict is not None:
-            jieba.load_userdict(user_dict)
+        self.user_dict = user_dict
+        if self.user_dict is not None:
+            jieba.load_userdict(self.user_dict)
 
-    def cut(self, word_str):
+    def cut(self, sentence):
         """
         Cut word string
 
-        @word_str: word string
+        @sentence: word string
 
         return list or None
+        ["word1", "word2" ...]
         """
         try:
-            seg_list = list(jieba.cut(word_str, cut_all=False, HMM=True))
+            seg_list = jieba.lcut(sentence, cut_all=False, HMM=True)
             return [item.encode('utf-8') for item in seg_list]
         except Exception,e:
-            logger.error('check is json exception:[%s]' % (str(e)))
+            logger.error('cut sentence:[%s] exception:[%s]' % (sentence, str(e)))
+            return None
+
+    def cut_with_weight(self, sentence, top_k=None):
+        """
+        Cut word string with weight
+
+        @sentence: word string
+        @top_k: return top k weight item, default return all
+
+        return list or None
+        ["word1`weight1", "word2`weight2" ...]
+        """
+        try:
+            if top_k is None:
+                top_k = 2147483647
+            seg_list = jieba.analyse.extract_tags(sentence, topK=top_k, withWeight=True)
+            return [(item[0].encode('utf-8'), item[1]) for item in seg_list]
+        except Exception,e:
+            logger.error('cut sentence:[%s] exception:[%s]' % (sentence, str(e)))
             return None
--- test/word_segment_test.py
+++ test/word_segment_test.py
@@ -11,9 +11,62 @@ import word_segment
 class WordSegmentTest(unittest.TestCase):
     def test_cut(self):
         ws_client = word_segment.WordSegment()
-        word_str = "每一行分三部分词语、词频（可省略）、词性（可省略），用空格隔开"
-        seg_list = ws_client.cut(word_str)
+        sentence = "每一行分三部分词语、词频（可省略）、词性（可省略），用空格隔开"
+        seg_list = ws_client.cut(sentence)
         print '\t'.join(seg_list)
+
+        sentence = "小明硕士毕业于中国科学院计算所，后在日本京都大学深造"
+        seg_list = ws_client.cut(sentence)
+        print '\t'.join(seg_list)
+
+        sentence = "李小福是创新办主任也是云计算方面的专家"
+        seg_list = ws_client.cut(sentence)
+        print '\t'.join(seg_list)
+
+        ws_client = word_segment.WordSegment("./data/user_dict.dat")
+        sentence = "每一行分三部分词语、词频（可省略）、词性（可省略），用空格隔开"
+        seg_list = ws_client.cut(sentence)
+        print '\t'.join(seg_list)
+
+        sentence = "小明硕士毕业于中国科学院计算所，后在日本京都大学深造"
+        seg_list = ws_client.cut(sentence)
+        print '\t'.join(seg_list)
+
+        sentence = "李小福是创新办主任也是云计算方面的专家"
+        seg_list = ws_client.cut(sentence)
+        print '\t'.join(seg_list)
+
+    def print_weight_list(self, seg_list):
+        for item in seg_list:
+            print item[0] + ":" + str(item[1]) + "\t",
+        print ""
+
+    def test_cut_with_weight(self):
+        ws_client = word_segment.WordSegment()
+        sentence = "每一行分三部分词语、词频（可省略）、词性（可省略），用空格隔开"
+        seg_list = ws_client.cut_with_weight(sentence)
+        self.print_weight_list(seg_list)
+
+        sentence = "小明硕士毕业于中国科学院计算所，后在日本京都大学深造"
+        seg_list = ws_client.cut_with_weight(sentence)
+        self.print_weight_list(seg_list)
+
+        sentence = "李小福是创新办主任也是云计算方面的专家"
+        seg_list = ws_client.cut_with_weight(sentence)
+        self.print_weight_list(seg_list)
+
+        ws_client = word_segment.WordSegment("./data/user_dict.dat")
+        sentence = "每一行分三部分词语、词频（可省略）、词性（可省略），用空格隔开"
+        seg_list = ws_client.cut_with_weight(sentence)
+        self.print_weight_list(seg_list)
+
+        sentence = "小明硕士毕业于中国科学院计算所，后在日本京都大学深造"
+        seg_list = ws_client.cut_with_weight(sentence)
+        self.print_weight_list(seg_list)
+
+        sentence = "李小福是创新办主任也是云计算方面的专家"
+        seg_list = ws_client.cut_with_weight(sentence)
+        self.print_weight_list(seg_list)
 
 if __name__ == '__main__':
     unittest.main()
