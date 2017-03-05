aae1db55acb01b74050378c8b7dc1636747d2478
cgl1079743846@gmail.com <cgl1079743846@gmail.com>
2017-03-05 09:55:54 +0800

    add mysql wrapper

---
+++ database/table_schema.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+import sys_path
+import mysql_conf
+
+# Table Class
+Class TestTable:
+    ID = 'id'
+    NAME = 'name'
--- database/test/mysql_conf_test.py
+++ database/test/mysql_conf_test.py
@@ -10,25 +10,12 @@ from mysql_conf import MysqlConfig
 class MysqlConfigTest(unittest.TestCase):
     def test_mysql_conf(self):
         mysql_config = MysqlConfig()
-        mysql_config.__debug_config__()
-
         mysql_config = MysqlConfig(hostname='localhost')
-        mysql_config.__debug_config__()
-
         mysql_config = MysqlConfig(hostname='localhost', port='3306')
-        mysql_config.__debug_config__()
-
         mysql_config = MysqlConfig(hostname='localhost', port='3306', user='chenguolin')
-        mysql_config.__debug_config__()
-
         mysql_config = MysqlConfig(hostname='localhost', port='3306', user='chenguolin', password='password')
-        mysql_config.__debug_config__()
-
         mysql_config = MysqlConfig(hostname='localhost', port='3306', user='chenguolin', password='password', db='test_db')
-        mysql_config.__debug_config__()
-
         mysql_config = MysqlConfig(hostname='localhost', port='3306', user='chenguolin', password='password', db='test_db', charset='utf8')
-        mysql_config.__debug_config__()
 
 if __name__ == '__main__':
     unittest.main()
